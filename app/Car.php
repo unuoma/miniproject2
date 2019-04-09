a1443ffbe823cdcd25c5cb9b734252f90456e9ce
unuoma <umo2@njit.edu>
2019-04-08 20:20:32 -0400

    4. New model called car and 3 properties to it: Make, Model, and year

---
+++ app/Car.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+
+    protected $table = 'cars';
+
+    protected $fillable = [
+        'make', 'model', 'year',
+    ];
+}
+
