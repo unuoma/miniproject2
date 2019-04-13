25be64769fda2f7e4ad2984c2452e1b4b0f8dddf
unuoma <umo2@njit.edu>
2019-04-13 16:15:00 -0400

    12. Update a user name Test

---
+++ tests/Unit/UpdateUserNameTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UpdateUserNameTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $data['name'] = 'Steve Smith';
+        $user = User::first();
+        $user->update($data);
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertEquals($data['name'], $user->name);
+        $this->assertTrue(true);
+    }
+}
