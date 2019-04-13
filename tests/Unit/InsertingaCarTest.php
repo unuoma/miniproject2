9d51266f644ca65e6650a19a0d64748a62a3d6f7
unuoma <umo2@njit.edu>
2019-04-13 16:44:37 -0400

    15. Inserting a car test

---
+++ tests/Unit/InsertingaCarTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class InsertingaCarTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+    public function testExample()
+    {
+        $data =  [
+            'make' => 'ford',
+            'model' => 'Test Model',
+            'year' => 2019,
+        ];
+        $car = Car::create($data);
+        $this->assertInstanceOf(Car::class, $car);
+        $this->assertEquals($data['make'], $car->make);
+        $this->assertEquals($data['model'], $car->model);
+        $this->assertEquals($data['year'], $car->year);
+    }
+}
