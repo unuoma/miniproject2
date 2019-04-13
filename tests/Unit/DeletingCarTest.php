6d583379ee1611a8f0af00da5815cfc057eea234
unuoma <umo2@njit.edu>
2019-04-13 17:39:56 -0400

    17. Deleting a car test

---
+++ tests/Unit/DeletingCarTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class DeletingCarTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $car = Car::first();
+        if($car)
+            $this->assertTrue($car->delete());
+    }
+}
