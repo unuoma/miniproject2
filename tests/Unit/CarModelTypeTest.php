a02112961410661de7366aee8802a824f1291042
unuoma <umo2@njit.edu>
2019-04-13 19:13:47 -0400

    20. Car model type test

---
+++ tests/Unit/CarModelTypeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class CarModelTypeTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        #factory(Car::class)->create();
+        $car =  Car::first();
+        $this->assertInternalType("string", $car->model);
+    }
+}
